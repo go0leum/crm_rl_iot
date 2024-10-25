f007dac092b5d3e25fe1ec152d9bf61627952fdd
go0leum <mijin575@konkuk.ac.kr>
2024-10-25 14:50:43 +0900

    Add:observation_space.py,README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# crm_rl_iot
-Construction Resource Management using Reinforcement Learning
+# Construction Resource Management using Reinforcement Learning
+
+## 프로젝트 소개
+
+- 건설 프로젝트에서 자원의 적절한 배분은 비용, 기간 및 품질에 직접적인 영향을 미치기 때문에 계획 프로세스에서 중요한 역할을 합니다.
+- 각 현장에서 필요한 자원과, 필요한 시가와 장소, 사용 가능 항목에 따라 다양한 건설 자원을 할당하는 것이 중요합니다.
+- 본 프로젝트에서는 [**Simulation of autonomous resource allocation through deep reinforcement learning-based portfolio-project integration**](https://doi.org/10.1016/j.autcon.2024.105381) 을 토대로 건설 현장 프로젝트에서 발생할 수 있는 task를 정의하고 강화 학습을 통해 최적의 장비 운용 프로세스 정책을 찾으려 합니다.
+- 건설 IoT 장비를 agent로 정의하고 건설 현장의 project들을 수행합니다. 제한된 자원에서 주어진 project를 수행하는데 걸리는 시간을 최소로 하는 최적 정책을 찾기 위한 강화 학습을 진행합니다.
---
+++ observation_space.py
@@ -0,0 +1,26 @@
+import numpy as np
+class ObservationSpace():
+    def __init__(self):
+        self.work_date # 작업일자
+        self.available_work_time # 작업시간 100 -> 0 (daily)
+        self.project_tasks = np.zeros((10, 3)) # 프로젝트 task 정보
+        # project1 : 0 (task1개)
+        # ...
+        # project7 : 0, 1, 2 (task3개)
+
+        self.project_tasks_status = np.zeros((10, 3)) # 프로잭트 task 상태
+        # project1 : 0 (task1 완료 안됨)
+        # ...
+        # project7 : 1, 0, 0 (task1만 완료됨)
+
+        self.project_tasks_remains = np.zeros((10, 3)) # 프로젝트 task 남은 시간
+        # project1 : 100 (task1 100시간 남음)
+        # ...
+        # project7 : 100, 200, 0 (task3만 완료됨)
+
+        self.project_tasks_resource = np.zeros(((10, 3, 4)))
+        # project1 : 1 (0, 0, 1, 1) (task1의 3번째 4번째 resource 준비됨)
+        # ...
+
+        self.agent_location = []
+        # agent의 x,y 좌표
