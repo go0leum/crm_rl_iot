d54ba73d43bb74433231b6d8ca5608ca1a5fb853
go0leum <mijin575@outlook.com>
2024-11-04 14:56:59 +0900

    Update

---
+++ environment.py
@@ -0,0 +1,79 @@
+import numpy as np
+import tkinter as tk
+import time
+from PIL import ImageTk, Image
+
+from data_loader import DataLoader
+
+PhotoImage = ImageTk.PhotoImage
+UNIT = 100
+WIDTH = 26
+HEIGHT = 12
+
+class Env(tk.Tk):
+    def __init__(self):
+        super(Env, self).__init__()
+        self.agent_location = np.zeros((1,2))
+        self.title('Construction Resource Management')
+        self.field_width, self.field_height, self.start, self.end, self.resource_dict, self.obstacle_dict = DataLoader.load_field_data()
+        self.project_dict, self.task_dict, self.material_dict, self.equipment_dict = DataLoader.load_project_data()
+
+    def step(self, action):
+        return 
+    
+    def get_obstacle_locations(self):
+        obstacle_locations = []
+        for key in self.obstacle_dict:
+            for location in self.obstacle_dict[key].locations:
+                obstacle_locations.append(location)
+        
+        return obstacle_locations
+
+class Action:
+
+    def __init__(self):
+        self.field_width = Env.field_width
+        self.field_height = Env.field_height
+        self.obstacle_location = Env.get_obstacle_locations()
+
+    def move_left(self, location):
+        new_location = np.array((location[0]-1, location[1]))
+
+        if new_location[0]<0:
+            new_location = location
+        elif new_location in self.obstacle_location:
+            new_location = location
+        
+        return new_location
+
+    def move_right(self, location):
+        new_location = np.array((location[0]+1, location[1]))
+
+        if new_location in self.obstacle_location:
+            new_location = location
+        elif new_location[0]>self.field_width:
+            new_location = location
+
+        return new_location
+      
+    def move_up(self, location):
+        new_location = np.array((location[0], location[1]-1))
+
+        if new_location[1]<0:
+            new_location = location
+        elif new_location in self.obstacle_location:
+            new_location = location
+        
+        return new_location
+
+    def move_down(self, location):
+        new_location = np.array((location[0], location[1]+1))
+
+        if new_location > self.field_height:
+            new_location = location
+        elif new_location in self.obstacle_location:
+            new_location = location
+        
+        return new_location
+    
+    def 
--- observation_space.py
+++ observation_space.py
@@ -1,42 +1,40 @@
 import numpy as np
-import tkinter as tk
-import time
-from PIL import ImageTk, Image
 
-class ObservationSpace():
-    def __init__(self):
-        self.transition_probability = 
-        self.work_date # 작업일자
-        self.available_work_time # 작업시간 100 -> 0 (daily)
-        self.project_tasks = np.zeros((10, 3)) # 프로젝트 task 정보
-        # project1 : 0 (task1개)
-        # ...
-        # project7 : 0, 1, 2 (task3개)
+class Obstacle:
+    def __init__(self, name):
+        self.name = name
+        self.locations = []
 
-        self.project_tasks_status = np.zeros((10, 3)) # 프로잭트 task 상태
-        # project1 : 0 (task1 완료 안됨)
-        # ...
-        # project7 : 1, 0, 0 (task1만 완료됨)
+class Resource:
+    def __init__(self, name, resource_list):
+        self.locations = []
+        self.name = name
+        self.resource_list = resource_list
+        self.resource_status = [True for i in range(len(resource_list))]
 
-        self.project_tasks_remains = np.zeros((10, 3)) # 프로젝트 task 남은 시간
-        # project1 : 100 (task1 100시간 남음)
-        # ...
-        # project7 : 100, 200, 0 (task3만 완료됨)
+class Project:
+    def __init__(self, name, task_list):
+        self.locations = []
+        self.name = name
+        self.task_list = task_list
+        self.status=True
 
-        self.project_tasks_resource = np.zeros(((10, 3, 4)))
-        # project1 : 1 (0, 0, 1, 1) (task1의 3번째 4번째 resource 준비됨)
-        # ...
+class Task:
+    def __init__(self, name, resource_list, work_hour):
+        self.name = name
+        self.resource_list = resource_list
+        self.work_hour = work_hour
+        self.status = True
 
-        self.agent_location = []
-        # agent의 x,y 좌표
+class Material:
+    def __init__(self, name, weight):
+        self.location = np.zeros((1,2))
+        self.name = name
+        self.weight = weight
+        self.status = True
 
-    def get_reward():
-
-        return self.reward()
-    
-    def state_after_action
-
-        return
-
-    def 
-
+class Equipment:
+    def __init__(self, name):
+        self.location = np.zeros((1,2))
+        self.name = name
+        self.status = True
