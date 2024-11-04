32330a7dc8eaf850f0eec77589a30944b4bfda2d
go0leum <mijin575@outlook.com>
2024-11-05 01:46:40 +0900

    update:observation_space.py

--- observation_space.py
+++ observation_space.py
@@ -10,31 +10,28 @@ class Resource:
         self.locations = []
         self.name = name
         self.resource_list = resource_list
-        self.resource_status = [True for i in range(len(resource_list))]
 
 class Project:
     def __init__(self, name, task_list):
         self.locations = []
         self.name = name
         self.task_list = task_list
-        self.status=True
+        self.status = False # project 수행 여부
 
 class Task:
     def __init__(self, name, resource_list, work_hour):
         self.name = name
         self.resource_list = resource_list
+        self.resource_status = [False for i in range(len(resource_list))] # resource 보유 여부
         self.work_hour = work_hour
-        self.status = True
+        self.status = False # task 수행 여부
 
 class Material:
     def __init__(self, name, weight):
-        self.location = np.zeros((1,2))
         self.name = name
         self.weight = weight
-        self.status = True
 
 class Equipment:
     def __init__(self, name):
         self.location = np.zeros((1,2))
         self.name = name
-        self.status = True
