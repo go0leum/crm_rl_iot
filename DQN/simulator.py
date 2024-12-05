f2747371540e37ad3f321b0400a264c37bd863c1
go0leum <mijin575@gmail.com>
2024-12-05 12:30:46 +0900

    simulator

--- DQN/simulator.py
+++ DQN/simulator.py
@@ -3,6 +3,7 @@ from tkinter import Button, Entry, Label
 import time
 import numpy as np
 from PIL import ImageTk, Image
+from stable_baselines3 import DQN
 
 PhotoImage = ImageTk.PhotoImage
 UNIT = 50
@@ -14,10 +15,14 @@ class GraphicDisplay(tk.Tk):
         self.field_width = self.env.field_width
         self.field_height = self.env.field_height
 
+        # 상태 인덱스 상수
+        self.IDX_POS_X = self.env.IDX_POS_X
+        self.IDX_POS_Y = self.env.IDX_POS_Y
+        self.IDX_PROJECT_START = self.env.IDX_PROJECT_START
+        self.IDX_PROJECT_END = self.env.IDX_PROJECT_END
+
         self.title('crm rl iot')
         self.geometry('{0}x{1}'.format(self.field_width * UNIT, self.field_height * UNIT + 100))
-        self.texts = []
-        self.arrows = []
 
         self.field_data, self.box_data = self.env.get_field_data() #start, resource, obstacle, project
         self.icons, self.boxes = self.load_images() #icon:agent,equipment, box:start, resource, obstacle, on_project, off_project
@@ -72,14 +77,14 @@ class GraphicDisplay(tk.Tk):
         canvas.pack()
 
         # work day 확인 창
-        self.workday = tk.StringVar()
-        self.workday.set('Remaining working day: '+ str(self.env.work_day))
-        workday_label = Label(self, textvariable=self.workday)
-        workday_label.place(x=self.field_width * UNIT * 0.05, y=(self.field_height * UNIT) + 40)
+        self.current_day = tk.StringVar()
+        self.current_day.set('current day: '+ str(self.env.current_day))
+        current_day_label = Label(self, textvariable=self.current_day)
+        current_day_label.place(x=self.field_width * UNIT * 0.05, y=(self.field_height * UNIT) + 40)
 
         # 버튼 초기화
         dqn_button = Button(self, text="simulator start",
-                            command=self.deep_q_learning)
+                            command=self.simulation)
         dqn_button.configure(width=15, activebackground="#33B5E5")
         canvas.create_window(self.field_width * UNIT * 0.5, self.field_height * UNIT + 10,
                              window=dqn_button)
@@ -89,8 +94,59 @@ class GraphicDisplay(tk.Tk):
     def render(self):
         time.sleep(0.1)
         self.canvas.tag_raise(self.agent_icon)
-        self.workday.set('Remaining working day: '+ str(self.env.work_day))
+        self.current_day.set('current day: '+ str(self.env.work_day))
         self.update()
+
+    def model_load(self, model_path):
+        try:
+            model = DQN.load(
+                model_path,
+                env=self.env,
+                tensorboard_log="./crm_rl_iot/DQN/log/dqn_tensorboard/",
+                # 필요한 경우 하이퍼파라미터 수정
+                learning_rate=1e-4,
+                exploration_fraction=0.2,
+                exploration_initial_eps=0.5,
+                exploration_final_eps=0.05
+            )
+            return model
+        
+        except Exception as e:
+            print(f"모델 로딩 중 오류 발생: {e}")
+            return None
+        
+    def simulation(self):
+        # 모델 load 하기
+        model_path = '../weights/simple_construction_dqn'
+        model = self.model_load(model_path)
+
+        done = False
+        # env의 observation space 가져오기
+        obs, _ = self.env.reset()
+        while not done:
+            # agent의 action 가져오기
+            action, _ = model.predict(obs, deterministic=True)
+            action = int(action)
+
+            # env의 step을 통해 다음 상태 가져오기
+            obs_prime, reward, done, _, _ = self.env.step(action)
+            agent_position = [obs[self.IDX_POS_X], obs[self.IDX_POS_Y]]
+
+            # agent icon move
+            self.canvas.move(self.agent_icon, (obs_prime[self.IDX_POS_X]-obs[self.IDX_POS_X])* UNIT, (obs_prime[self.IDX_POS_Y]-obs[self.IDX_POS_Y])* UNIT)
+            
+            # project icon off
+            if agent_position in self.env.project_positions :
+                project_idx = self.env.project_positions.index(agent_position)
+                base_index = self.IDX_PROJECT_START+(4*(project_idx-1))
+                proj_task1_complete = self.state[base_index] == 2
+                proj_task2_complete = (self.state[base_index + 2] == 2 and self.state[base_index + 3] == 2)
+                
+                if (proj_task1_complete and proj_task2_complete):
+                    #self.canvas.itemconfig(self.project_box_dict[field_name], image=self.boxes[4])
+        
+            obs = obs_prime
+            self.render()
 
 if __name__ == "__main__":
     grid_world = GraphicDisplay()
